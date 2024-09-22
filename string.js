9c5731b47da271ab655c5d35a46a4bbf79943588
Ankita-Pramanik <ankitapramanik2003@gmail.com>
2024-09-23 01:58:37 +0530

    second commit

---
+++ string.js
@@ -0,0 +1,39 @@
+const string1 = new String ("Hiteshch is a good boy");
+console.log(string1.indexOf("is"));
+
+console.log(string1.charAt(5));
+
+const str1="Ankita";
+const str2="  Joy sarada  ";
+console.log(str1.concat("",str2));
+
+console.log(str1.includes("Ankita"));
+
+console.log(str1.replace('k',2));
+
+console.log(string1.length);
+
+console.log(str1.toLowerCase());
+
+console.log(str2.toUpperCase());
+
+console.log(str2.trim());
+
+console.log(str1.substring(2,4));
+
+console.log(string1.split(string1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ var.js
@@ -0,0 +1,14 @@
+const accountId = 12241
+let accountEmail = " ankita2003@gmail.com "
+var accountPassword = " 32495 "
+accountCity = " Kolkata "
+
+//accountId = 45526
+
+console.log( accountId );
+
+accountEmail = " ankita2002@gmail.com "
+accountPassword = " 32499 "
+accountCity = " Bangalore "
+
+console.table([ accountPassword , accountCity , accountEmail ])
